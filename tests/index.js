5ccbd9dfff1c9c7489359c9a7ec065b82a8cb0c1
marco.tulio <marco.tulio@razem.local>
2019-11-07 09:56:36 -0300

    feat: added a function that gets the authentication token from the user specified

--- tests/index.js
+++
@@ -1,8 +0,0 @@
-const eadbox = require("../src/eadbox-api")
-
-async function start() {
-  const responseInsert = await eadbox.registerUser(apiObject, userObject);
-  console.log(responseInsert);
-}
-
-start();
