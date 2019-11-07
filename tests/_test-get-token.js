9f39a44832fb799ef470fcad6fca55d1e21598ca
marco.tulio <marco.tulio@razem.local>
2019-11-07 10:05:30 -0300

    feat: refactored function names to be more clear

--- tests/_test-get-token.js
+++ tests/_test-get-token.js
@@ -2,7 +2,7 @@ const eadbox = require("../src/eadbox-api")
 const models = require('./models')
 
 async function start() {
-  const jsonGetToken = await eadbox.getAuthTokenFromLogin('https://unispk.com.br', models.emailAndPassword);
+  const jsonGetToken = await eadbox.getUserAuthTokenFromLogin('https://unispk.com.br', models.emailAndPassword);
 
   console.log(jsonGetToken);
 }
