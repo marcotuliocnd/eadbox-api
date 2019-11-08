02beb8234a6c9d958812d87cab6d51b8f06b75c9
marco.tulio <marco.tulio@razem.local>
2019-11-08 09:08:52 -0300

    feat: created function to get user_id

--- tests/_test-get-token.js
+++ tests/_test-get-token.js
@@ -2,21 +2,14 @@ const eadbox = require("../src/eadbox-api")
 const models = require('./models')
 
 async function start() {
-  try {
-      const marcoAuthToken = await eadbox.getUserAuthTokenFromLogin('https://unispk.com.br', models.emailAndPassword);
-      console.log('Peguei o token: ' + marcoAuthToken);
-  } catch (err) {
-    console.log('Algo deu errado na hora de pegar o token =(');
-  }
+    const adminToken = await eadbox.getUserAuthTokenFromLogin('https://unispk.com.br', models.adminEmailAndPassword);
+    console.log('Peguei o token do admin: ' + adminToken);
 
-  try {
-      const addedCourse = await eadbox.addCourseForUser('https://unispk.com.br', 'financeiro-gestor', this.marcoAuthToken);
-      console.log("Curso foi adicionado com sucesso");
-  } catch (err) {
-    console.log(err)
-  }
+    const marcoInfo = await eadbox.makeLoginFromEmailAndPassword('https://unispk.com.br', models.emailAndPassword);
+    console.log(marcoInfo);
 
-
+    const addedCourse = await eadbox.addCourseForUser('https://unispk.com.br', 'financeiro-gestor', marcoAuthToken);
+    console.log("Curso foi adicionado com sucesso");
 }
 
 start();
